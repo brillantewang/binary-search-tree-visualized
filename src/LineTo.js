2b1f706c53e2621fc97d2d03c226ad1b8f6b5450
Brillante Wang <brillante.wang@gmail.com>
2020-01-09 00:53:41 -0800

    use trigonometry to reposition line endpoints to be at edges instead of centers of nodes

--- src/LineTo.js
+++ src/LineTo.js
@@ -14,17 +14,31 @@ const StyledSvg = styled.svg`
   width: 100%;
 `;
 
-
 const LineTo = ({ fromPos, toPos }) => {
   const x1 = fromPos.x + (fromPos.width / 2);
   const y1 = fromPos.y + (fromPos.height / 2);
   const x2 = toPos.x + (toPos.width / 2);
   const y2 = toPos.y + (toPos.height / 2);
 
+  const yDelta = Math.abs(y2 - y1);
+  const xDelta = Math.abs(x2 - x1);
+
+  const angle = Math.atan(yDelta / xDelta);
+  const radius = fromPos.height / 2;
+
+  const offsetY = radius * Math.sin(angle);
+  const offsetX = radius * Math.cos(angle);
+
+  const newX1 = x1 < x2 ? x1 + offsetX : x1 - offsetX;
+  const newY1 = y1 + offsetY;
+
+  const newX2 = x2 < x1 ? x2 + offsetX : x2 - offsetX;
+  const newY2 = y2 - offsetY;
+
   return (
     <FixedContainer>
       <StyledSvg>
-        <line strokeWidth="2px" stroke="red" x1={x1} y1={y1} x2={x2} y2={y2} />
+        <line strokeWidth="2px" stroke="white" x1={newX1} y1={newY1} x2={newX2} y2={newY2} />
       </StyledSvg>
     </FixedContainer>
   )
